11e41a3591fe15d8b4ebd79da1699be4aac31483
Rogério Carvalho Schneider <stockrt@gmail.com>
2015-05-27 19:06:46 -0300

    Fstab timeout and noauto

--- vbb.sh
+++ vbb.sh
@@ -98,6 +98,9 @@ chmod 0700 ~/.ssh ;\
 chmod 0600 ~/.ssh/authorized_keys
 "
 
+# Fstab timeout and noauto.
+sed -i 's/timeo=.*/timeo=14,noauto 0 0/g' /etc/fstab
+
 # Clean history.
 rm -f /home/vagrant/.bash_history
 history -c
