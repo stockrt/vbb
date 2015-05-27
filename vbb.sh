1fc3b3ffc0b0adc7e95a0be0e463008bc4f42704
Rogério Carvalho Schneider <stockrt@gmail.com>
2015-05-27 19:05:58 -0300

    Update date format

--- vbb.sh
+++ vbb.sh
@@ -3,7 +3,7 @@
 # Vagrant Base Box (vbb) setup steps for CentOS/RedHat/Fedora.
 #
 # Author: Rogério Schneider <stockrt@gmail.com>
-# Date: 27/05/2015
+# Date: May 27, 2015
 
 puts () {
     # Pretty print.
